d44945db1ba81d1e7210a990a9f5da4d441ead46
Arif <arifcodes11@gmail.com>
2025-03-22 22:30:57 +0530

    add Go program to demonstrate variable declaration and output

---
+++ Day2/decVar.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Alice"
+	age := 25
+
+	var height float64 = 6.01
+	var isStudent bool = true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Student:", isStudent)
+}
