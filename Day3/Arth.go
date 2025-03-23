ecb98c69543c7b0632c8dbca8b44617d055e42df
Arif <arifcodes11@gmail.com>
2025-03-23 23:51:41 +0530

    add Go program to demonstrate basic arithmetic operations

---
+++ Day3/Arth.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+
+	fmt.Println("Addition : ", a+b)
+	fmt.Println("Subtraction : ", a-b)
+	fmt.Println("Multipliaction : ", a*b)
+	fmt.Println("Modulus: ", a%b)
+	fmt.Println("Division: ", a/b)
+}
