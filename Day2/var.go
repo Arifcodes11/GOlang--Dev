1363f1768c21faf714a4a38f3ed4a8dfc12b4b65
Arif <arifcodes11@gmail.com>
2025-03-23 17:38:10 +0530

    add Go program to demonstrate variable declaration and output for city and country

---
+++ Day2/var.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var city, country string = "New York", "USA" //Declaring two variables
+	fmt.Println("City:", city)
+	fmt.Println("Country:", country)
+}
