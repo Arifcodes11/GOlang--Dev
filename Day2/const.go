2ee3165e9635e7cafd7079d630a3d0a99f30247a
Arif <arifcodes11@gmail.com>
2025-03-23 23:42:33 +0530

    update const.go to enhance comment clarity for the PI constant

--- Day2/const.go
+++ Day2/const.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	const PI float64 = 3.14159    //Explicit type
+	const PI float64 = 3.14159    //Explicit type,A constant that holds the value of Pi
 	const Greeting = "Hello, Go!" //implicit type
 
 	fmt.Println("PI:", PI)
