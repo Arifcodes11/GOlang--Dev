460e0995226035674555f900922a2e892b582181
Arifcodes11 <arifcodes11@gmail.com>
2025-03-25 00:44:49 +0530

    add Go program to demonstrate comparison operators

---
+++ Day3/comp.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 5
+
+	fmt.Println("a==b:", a == b)
+	fmt.Println("a!=b:", a != b)
+	fmt.Println("a > b:", a > b)
+	fmt.Println("a < b:", a < b)
+	fmt.Println("a<=b:", a >= b)
+	fmt.Println("a<=b:", a <= b)
+
+}
