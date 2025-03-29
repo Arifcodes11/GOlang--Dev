55b5c90405461cf44aea8cb6e584a7ac9a4589e9
Arifcodes11 <arifcodes11@gmail.com>
2025-03-30 00:07:06 +0530

    refactor Fibonacci program for improved formatting and readability

--- Day5/fib.go
+++ Day5/fib.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 	var n int
-	fmt.Print("Enter the number of terms: ")
+	fmt.Print("Enter the number of terms : ")
 	fmt.Scan(&n)
 
-	a, b := 0, 1 // First two terms
+	a,b:=0,1
 
-	fmt.Println("Fibonacci Series:")
-	for i := 0; i < n; i++ {
+	fmt.Println("fibonacci series:")
+	for i:=0;i<n;i++{
 		fmt.Println(a)
-		a, b = b, a+b // Update a and b for next iteration
+		a,b=b,a+b
 	}
 }
