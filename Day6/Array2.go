01c8da726b4caf24862fc4db5b09ff93f0c7cb37
Arifcodes11 <arifcodes11@gmail.com>
2025-03-31 23:15:28 +0530

    add Go program to demonstrate shorter array initialization

---
+++ Day6/Array2.go
@@ -0,0 +1,11 @@
+//Shorter Initialization
+//You can declare and initialize an array at the same time
+
+package main
+
+import "fmt"
+
+func main() {
+	fruits := [4]string{"Apple", "Banana", "Orange", "grapes"}
+	fmt.Println("fruits:", fruits)
+}
